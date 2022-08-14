ef429ee90bacfba565615e43e452ab64a9cef2a5
楹浩 胡 <mothanbourgavalon@outlook.com>
2022-08-14 18:19:24 +0800

    完善了链表

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,63 @@ pub struct LinkedList<T> {
 
 impl<T> LinkedList<T> {
     pub fn new() -> LinkedList<T> {
-        LinkedList { head: None, tail: None, size: 0 }
+        LinkedList {
+            head: None,
+            tail: None,
+            size: 0,
+        }
+    }
+
+    pub fn front(&self) -> &T {
+        if let None = &self.head {
+            panic!("");
+        } else {
+            &self.head.as_ref().unwrap().item
+        }
+    }
+
+    pub fn push_head(&mut self, item: T) {
+        if self.size == 0 {
+            let new_node = Rc::new(Node {
+                item,
+                prev: RefCell::new(Weak::new()),
+                next: RefCell::new(None),
+            });
+            self.tail = Some(Rc::clone(&new_node));
+            self.head = Some(Rc::clone(&new_node));
+            self.size += 1;
+            return;
+        }
+
+        let new_node = Rc::new(Node {
+            item,
+            prev: RefCell::new(Weak::new()),
+            next: RefCell::new(Some(Rc::clone(&self.head.as_ref().unwrap()))),
+        });
+        *self.head.as_ref().unwrap().prev.borrow_mut() = Rc::downgrade(&new_node);
+        self.head = Some(new_node);
+        self.size += 1;
+    }
+
+    pub fn pop_head(&mut self) {
+        if self.size == 0 {
+            panic!("");
+        }
+
+        if let Some(node) = &*self.head.clone().as_ref().unwrap().next.borrow() {
+            self.head = Some(Rc::clone(node));
+            self.size -= 1;
+        } else {
+            self.head = None;
+            self.tail = None;
+            self.size -= 1;
+        }
     }
 
     pub fn back(&self) -> &T {
         if let None = &self.tail {
             panic!("");
-        }
-        else {
+        } else {
             &self.tail.as_ref().unwrap().item
         }
     }
@@ -51,7 +100,7 @@ impl<T> LinkedList<T> {
         });
 
         *self.tail.as_ref().unwrap().next.borrow_mut() = Some(Rc::clone(&new_node));
-        self.tail = Some(Rc::clone(&new_node));
+        self.tail = Some(new_node);
         self.size += 1;
     }
 
@@ -59,12 +108,12 @@ impl<T> LinkedList<T> {
         if self.size == 0 {
             panic!("");
         }
+
         if let Some(node) = self.tail.clone().unwrap().prev.borrow().upgrade() {
             *node.next.borrow_mut() = None;
             self.tail = Some(node);
             self.size -= 1;
-        }
-        else {
+        } else {
             self.head = None;
             self.tail = None;
             self.size -= 1;
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,14 @@ use linked_list::LinkedList;
 
 fn main() {
     let mut list = LinkedList::new();
-    list.push_back(3);
-    list.push_back(20);
-    push(&mut list, 100);
-    println!("{:?}", &list);
-    list.pop_back();
-    list.pop_back();
-    list.push_back(50);
-    println!("{:?}", &list);
-}
 
-fn push(list: &mut LinkedList<i32>, i: i32) {
-    list.push_back(i);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    list.pop_head();
+    list.push_head(6);
+    list.pop_head();
+    list.push_back(7);
+    list.pop_back();
+    println!("{:?}", &list);
 }
